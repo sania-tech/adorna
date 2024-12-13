ef694f5b7c6b008642b1fac4de80e196f9d54f2a
Sania-tech <saniasohail782@gmail.com>
2024-12-13 19:01:16 +0100

    Added all packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import Navbar from './componets/navbar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './componets/Navbar/'
 
 const App = () => {
   return (
     <div classname='px-4 sm:px-[5vw] md:px-[7vw] 1g:px-[9vw]'>
-      <navbar/> 
+      
+      
 
-      <Router>
+      
+      <Navbar/> 
+
+     
+        <Routes>
+
         <Route path='/' element ={<home/>}/>
-        <Router path = '/collection' element = {<collection/>}/>
-        <Router path = '/about' element = {<about/>}/>
-        <Router path = '/contact' element = {<contact/>}/>
-        <Router path = '/product/:productId' element = {<product/>}/>
-        <Router path = '/cart' element = {<cart/>}/>
-        <Router path = '/login' element = {<login/>}/>
-        <Router path = '/place-order' element = {<placeorder/>}/>
-        <Router path = '/orders' element = {<orders/>}/>
+        <Route path = '/collection' element = {<collection/>}/>
+        <Route path = '/about' element = {<about/>}/>
+        <Route path = '/contact' element = {<contact/>}/>
+        <Route path = '/product/:productId' element = {<product/>}/>
+        <Route path = '/cart' element = {<cart/>}/>
+        <Route path = '/login' element = {<login/>}/>
+        <Route path = '/place-order' element = {<placeorder/>}/>
+        <Route path = '/orders' element = {<orders/>}/>
+        </Routes>
 
 
 
-      </Router>
+      
 
     </div>
   )
