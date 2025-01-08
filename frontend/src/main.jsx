13fa558efdc0380d127b57e6d9dae9eb0b3bd76c
Sania-tech <saniasohail782@gmail.com>
2025-01-08 16:35:34 +0100

    changing some mistakes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,8 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ShopContextProvider>
       <App />
-    </ShopContextProvider>console.log('Rendering App component')
-console.log('Initializing ShopContextProvider')
-console.log('Setting up BrowserRouter')
+    </ShopContextProvider>
   </BrowserRouter>,
 )
