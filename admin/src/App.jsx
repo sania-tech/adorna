b3357c277bbcc7b0ea997211da015d89bb3cf21d
Sania-tech <saniasohail782@gmail.com>
2025-01-25 20:19:55 +0100

    fixed admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,6 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 // Environment variables for backend URL and currency symbol
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
+const backendUrl = "https://adorna-backend.vercel.app"
 export const currency = '$'
 
 /**
